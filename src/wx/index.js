e1ebdd840694afba4d4f51047a0e44493509e955
haojy <haojiayin@gmail.com>
2018-01-09 00:03:33 +0800

    ADD promisify wx game API

--- src/wx/index.js
+++ src/wx/index.js
@@ -1,6 +1,6 @@
 /* global wx*/
 import promisify, { promisifyReturns } from '../util/promisify'
-const exceptions = `closeSocket
+const exceptProgramAPI = `closeSocket
 stopRecord
 getRecorderManager
 pauseVoice
@@ -31,36 +31,65 @@ createCameraContext
 createLivePlayerContext
 createLivePusherContext`
 
+const exceptGameAPI = `createImage
+loadFont
+setPreferredFramesPerSecond
+getFileSystemManager`
 
 const promisifiedWxApi = promisify(wx, {
   objectParams: true,
   exclude: [
     /^on/,
+    /^off/,
     /Sync$/,
-    new RegExp(exceptions.split(/\r\n|\r|\n/).join('|'), 'gi'),
+    new RegExp(exceptProgramAPI.split(/\r\n|\r|\n/).join('|'), 'gi'),
+    new RegExp(exceptGameAPI.split(/\r\n|\r|\n/).join('|'), 'gi'),
   ],
 })
 
-promisifiedWxApi.createCameraContext = promisifyReturns(wx.createCameraContext.bind(wx), {
-  takePhoto: { objectParams: true },
-  startRecord: { objectParams: true },
-  stopRecord: { objectParams: true },
-})
+if (wx.createCameraContext) {
+  promisifiedWxApi.createCameraContext = promisifyReturns(wx.createCameraContext.bind(wx), {
+    takePhoto: { objectParams: true },
+    startRecord: { objectParams: true },
+    stopRecord: { objectParams: true },
+  })
+}
 
-promisifiedWxApi.createLivePlayerContext = promisifyReturns(wx.createLivePlayerContext.bind(wx), {
-  play: { objectParams: true },
-  stop: { objectParams: true },
-  mute: { objectParams: true },
-  requestFullScreen: { objectParams: true },
-  exitFullScreen: { objectParams: true },
-})
+if (wx.createLivePlayerContext) {
+  promisifiedWxApi.createLivePlayerContext = promisifyReturns(wx.createLivePlayerContext.bind(wx), {
+    play: { objectParams: true },
+    stop: { objectParams: true },
+    mute: { objectParams: true },
+    requestFullScreen: { objectParams: true },
+    exitFullScreen: { objectParams: true },
+  })
+}
 
-promisifiedWxApi.createLivePusherContext = promisifyReturns(wx.createLivePusherContext.bind(wx), {
-  play: { objectParams: true },
-  stop: { objectParams: true },
-  mute: { objectParams: true },
-  requestFullScreen: { objectParams: true },
-  exitFullScreen: { objectParams: true },
-})
+if (wx.createLivePusherContext) {
+  promisifiedWxApi.createLivePusherContext = promisifyReturns(wx.createLivePusherContext.bind(wx), {
+    play: { objectParams: true },
+    stop: { objectParams: true },
+    mute: { objectParams: true },
+    requestFullScreen: { objectParams: true },
+    exitFullScreen: { objectParams: true },
+  })
+}
 
+if (wx.getFileSystemManager) {
+  promisifiedWxApi.getFileSystemManager = promisifyReturns(wx.getFileSystemManager.bind(wx), {
+    access: { objectParams: true },
+    copyFile: { objectParams: true },
+    getFileInfo: { objectParams: true },
+    mkdir: { objectParams: true },
+    rmdir: { objectParams: true },
+    rename: { objectParams: true },
+    readFile: { objectParams: true },
+    readdir: { objectParams: true },
+    saveFile: { objectParams: true },
+    stat: { objectParams: true },
+    writeFile: { objectParams: true },
+    unlink: { objectParams: true },
+  })
+}
+// if (wx.connectSocket) 
 export default promisifiedWxApi
